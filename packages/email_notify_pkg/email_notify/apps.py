b3c7928724199ca8a4749aa40390f51bbba91167
hosseinirtr <h.hosseini.me@gmail.com>
2025-06-27 16:13:21 +0330

    Rename class "Email_notifyConfig" to match the regular expression ^_?([A-Z_][a-zA-Z0-9]*|[a-z_][a-z0-9_]*)$.

--- packages/email_notify_pkg/email_notify/apps.py
+++ packages/email_notify_pkg/email_notify/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Email_notifyConfig(AppConfig):
+class EmailNotifyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "email_notify"
